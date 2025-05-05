9277f108e8c4f11266ce121bcb65fbe572afd06c
qwerg21 <ikanin.vasiliy@gmail.com>
2025-05-05 23:11:16 +0000

    df

---
+++ scripts/mine.ts
@@ -0,0 +1,11 @@
+import {Address} from '@ton/ton';
+
+const walletAddress = Address.parse('0QDZc-I34wx76zL5U6bj7cHUsx5imSSCUTP_Hn40Tjwn51oy');
+const collectionAddress = Address.parse('EQDk8N7xM5D669LC2YACrseBJtDyFqwtSPCNhRWXU7kjEptX');
+
+async function mine () {
+
+
+}
+
+mine();
